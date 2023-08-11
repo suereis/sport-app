dece55bf4cda17263e96064a27a9b4eab4625857
Mikiko-01596985 <125385643+Mikiko-01596985@users.noreply.github.com>
2023-08-11 17:37:44 -0400

    log in and register pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,54 @@
 import React from "react";
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login logic here
+    console.log('Login form submitted:', formData);
+  };
+
   return (
-    <div style={{ margin: "100px" }}>
+    <div style={{ margin: '100px' }}>
       <div>Login</div>
-      <button
-        className="primaryBtn"
-        onClick={() => {
-          navigate("/register");
-        }}
-      >
-        Register
-      </button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
